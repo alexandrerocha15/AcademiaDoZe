019554647a0c568cf78882eef4580ef07012b76a
Alexandre Rocha <juny14_15@hotmail.com>
2026-08-11 00:37:39 -0300

    DEV04 - testes unitarios e ajustes no dominio

--- AcademiaDoZe.Domain/ValueObjects/Cpf.cs
+++ AcademiaDoZe.Domain/ValueObjects/Cpf.cs
@@ -35,7 +35,29 @@ public record Cpf
         if (cpf.Length != 11)
             return false;
 
-        return true;
+        if (cpf.Distinct().Count() == 1)
+            return false;
+
+        int soma = 0;
+
+        for (int i = 0; i < 9; i++)
+            soma += (cpf[i] - '0') * (10 - i);
+
+        int resto = soma % 11;
+        int digito1 = resto < 2 ? 0 : 11 - resto;
+
+        if ((cpf[9] - '0') != digito1)
+            return false;
+
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+            soma += (cpf[i] - '0') * (11 - i);
+
+        resto = soma % 11;
+        int digito2 = resto < 2 ? 0 : 11 - resto;
+
+        return (cpf[10] - '0') == digito2;
     }
 
     public override string ToString() => Valor;
